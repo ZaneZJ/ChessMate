43bd26cd5aa9e6e31ea099626dc580ddc38dd66e
Zane <76492358+ZaneZJ@users.noreply.github.com>
2023-05-07 12:48:59 +0300

    Update 07/05

--- app/src/main/java/com/example/chessmate/Activity/StartActivity.java
+++ app/src/main/java/com/example/chessmate/Activity/StartActivity.java
@@ -3,6 +3,9 @@ package com.example.chessmate.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
@@ -12,18 +15,26 @@ import com.example.chessmate.R;
 
 public class StartActivity extends AppCompatActivity  {
 
-    private ConstraintLayout startBtn;
+    private Button startBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        // Hide the title bar and show the activity in full screen
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getSupportActionBar().hide();
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        setContentView(R.layout.activity_start);
 
         startBtn = findViewById(R.id.startbtn);
         startBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(StartActivity.this, MainActivity.class));
+
             }
         });
     }
